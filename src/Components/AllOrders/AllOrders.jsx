0e931690c672b2d87b51d249a7ad0bd0c6567695
ahmed samir <ahmedSamir@gmail.com>
2024-08-04 15:19:11 +0300

    handle allOrders page

--- src/Components/AllOrders/AllOrders.jsx
+++ src/Components/AllOrders/AllOrders.jsx
@@ -6,7 +6,10 @@ export default function AllOrders() {
     <>
       <div className="container">
         <div className="bg-success p-5 my-5 text-center text-white">
-          <h2>Congrats</h2>
+          <h3>
+            Your order was successfully placed! Thank you for shopping with
+            us.We'll notify you once it's on its way.
+          </h3>
         </div>
       </div>
     </>
